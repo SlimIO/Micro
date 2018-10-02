01f78ab90ea3f3bf19538b0014dbc51c3a13a793
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-10-02 11:24:31 +0200

    Improve test return

--- test/test.js
+++ test/test.js
@@ -8,15 +8,15 @@ const Micro = require("..");
 avaTest("Micro.now()", (assert) => {
     const ts = Micro.now();
     assert.true(is.number(ts));
-    assert.true(ts.toString().length === 16);
+    assert.is(ts.toString().length, 16);
 });
 
 avaTest("Micro.gettimeofday()", (assert) => {
     const timeval = Micro.gettimeofday();
     assert.true(is.plainObject(timeval));
-    assert.true(Object.keys(timeval).length === 2);
+    assert.is(Object.keys(timeval).length, 2);
     assert.true(is.number(timeval.sec));
     assert.true(is.number(timeval.usec));
-    assert.true(timeval.sec.toString().length === 10);
-    assert.true(timeval.usec.toString().length === 6);
+    assert.is(timeval.sec.toString().length, 10);
+    assert.is(timeval.usec.toString().length, 6);
 });
