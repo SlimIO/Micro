4eb6dd051a7c294dd3df9994408d7b25ff297f3b
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-10-25 11:57:56 +0200

    test: complete test for new features

--- test/test.js
+++ test/test.js
@@ -11,6 +11,12 @@ avaTest("Micro.now()", (assert) => {
     assert.is(ts.toString().length, 16);
 });
 
+avaTest("Micro.clock.now()", (assert) => {
+    const ts = Micro.clock.now();
+    assert.true(is.number(ts));
+    assert.is(ts.toString().length, 16);
+});
+
 avaTest("Micro.gettimeofday()", (assert) => {
     const timeval = Micro.gettimeofday();
     assert.true(is.plainObject(timeval));
@@ -19,4 +25,14 @@ avaTest("Micro.gettimeofday()", (assert) => {
     assert.true(is.number(timeval.usec));
     assert.is(timeval.sec.toString().length, 10);
     assert.is(timeval.usec.toString().length, 6);
+});
+
+avaTest("Micro.clock.gettime()", (assert) => {
+    const timespec = Micro.clock.gettime();
+    assert.true(is.plainObject(timespec));
+    assert.is(Object.keys(timespec).length, 2);
+    assert.true(is.number(timespec.sec));
+    assert.true(is.number(timespec.nsec));
+    assert.is(timespec.sec.toString().length, 6);
+    assert.is(timespec.nsec.toString().length, 9);
 });
